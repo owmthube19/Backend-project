f63cd97bf631475257573b2fb84fed2e7247e80a
Om Thube <OMTHUBE51@GMAIL.COM>
2026-09-09 14:49:36 +0530

    Add new user routes for password change, current user retrieval, account updates, avatar and cover image uploads, user channel profile access, and watch history retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser } from "../controllers/user.controllers.js";
+import { 
+    loginUser, 
+    registerUser, 
+    logoutUser, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/Multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controllers.js";
@@ -25,5 +36,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post( verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
 
 export default router;
